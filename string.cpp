c11f9a3bc463319f6cf3397df60173b2a015bd24
bhanuseshukumar <30802522+bhanuseshukumar@users.noreply.github.com>
2024-10-06 17:43:51 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -4,3 +4,7 @@ std::deque<std::string> deque1(
     (std::istream_iterator<std::string>(iss1)),
     std::istream_iterator<std::string>());
 //prefix and suffix analysis
+
+getline(ss,s,' ') // getline(istream, var2copy, delimeter)
+
+s.find(delimeter_str, start_pos);
